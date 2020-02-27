cb2af694da2fdf6ac09784df41b672db16b7311e
Joao Pedro M. Trabbold <jpmtrabbold@gmail.com>
2020-02-27 19:34:07 +1300

    .

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -8,13 +8,15 @@ import { Main } from 'features/Main'
 import { setModalTheme } from 'components/material-ui-modals'
 import Utils from '@date-io/date-fns'
 import MuiPickersUtilsProvider from '@material-ui/pickers/MuiPickersUtilsProvider'
+import { DemoMain } from 'demo-features/DemoMain'
 
 setModalTheme(theme)
 const App = () => {
     return (
         <MuiThemeProvider theme={theme}>
             <MuiPickersUtilsProvider utils={Utils}>
-                <Main />
+                {/* <Main /> */}
+                <DemoMain/>
             </MuiPickersUtilsProvider>
         </MuiThemeProvider>
     )
